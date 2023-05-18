abdcc2edcd265679fd2cb2e422f2509ee88780df
Yohanes Velly <yohanesvelly23@gmail.com>
2023-05-19 03:26:03 +0700

    initiating button proptypes

--- src/elements/Button/index.js
+++ src/elements/Button/index.js
@@ -1,5 +1,49 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import propTypes from 'prop-types';
 
 export default function Button(props) {
+  const className = [props.className];
+  if (props.isPrimary) className.push('btn-primary');
+  if (props.isLarge) className.push('btn-lg');
+  if (props.isSmall) className.push('btn-sm');
+  if (props.isBlock) className.push('btn-block');
+  if (props.hasShadow) className.push('btn-shadow');
+
+  const onClick = () => {
+    if (props.onClick) props.onClick();
+  };
+
+  if (props.type === 'link') {
+    if (props.isExternal) {
+      return (
+        <a
+          href={props.href}
+          className={className.join(' ')}
+          style={props.style}
+          target={props.target === '_blank' ? '_blank' : undefined}
+          rel={props.target === '_blank' ? 'noopener noreferrer' : undefined}
+        >
+          {props.children}
+        </a>
+      );
+    } else {
+    }
+  }
   return <div></div>;
 }
+
+Button.propTypes = {
+  type: propTypes.oneOf(['button', 'link']),
+  onClick: propTypes.func,
+  href: propTypes.string,
+  target: propTypes.string,
+  className: propTypes.string,
+  isDisbled: propTypes.bool,
+  isLoading: propTypes.bool,
+  isSmall: propTypes.bool,
+  isLarge: propTypes.bool,
+  isBlock: propTypes.bool,
+  isExternal: propTypes.bool,
+  hasShadow: propTypes.bool,
+};
