709b79eca2970922f48129137df3afb1f751aecb
Yohanes Velly <yohanesvelly23@gmail.com>
2023-05-22 01:33:29 +0700

    update react router dom config to v6, unfinished styling header and apply prettier, try to run landing page, and troubleshooting wrong nav placement and implement rrd v6

--- src/parts/Header.js
+++ src/parts/Header.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import Button from '../elements/Button';
 import BrandIcon from './BrandIcon';
+import { useLocation } from 'react-router-dom'
 
-export default function Header(props) {
+export default function Header() {
+
+  const location = useLocation()
   const getNavLinkClass = (path) => {
-    return props.location.pathname === path ? ' active' : '';
+    return location.pathname === path ? ' active' : '';
   };
 
   return (
@@ -12,53 +15,54 @@ export default function Header(props) {
       <div className="container">
         <nav className="navbar navbar-expand-lg navbar-light">
           <BrandIcon />
+
+          <div className="collapse navbar-collapse">
+            <ul className="navbar-nav mr-auto">
+              <li className={`nav-item${getNavLinkClass('/')}`}>
+                <Button className="nav-link" type="link" href="/">
+                  Beranda
+                </Button>
+              </li>
+              <li className={`nav-item${getNavLinkClass('/pencarian')}`}>
+                <Button className="nav-link" type="link" href="/pencarian">
+                  Pencarian
+                </Button>
+              </li>
+              <li className={`nav-item${getNavLinkClass('/tentang')}`}>
+                <Button className="nav-link" type="link" href="/tentang">
+                  Tentang
+                </Button>
+              </li>
+            </ul>
+            <li className="nav-item dropdown">
+              <a
+                className="nav-link dropdown-toggle"
+                href="/"
+                role="button"
+                data-toggle="dropdown"
+                aria-expanded="false"
+              >
+                Dropdown
+              </a>
+              <div className="dropdown-menu">
+                <Button
+                  className={`dropdown-item${getNavLinkClass('/history')}`}
+                  type="link"
+                  href="/history"
+                >
+                  History
+                </Button>
+                <Button
+                  className={`dropdown-item${getNavLinkClass('/logout')}`}
+                  type="link"
+                  href="/logout"
+                >
+                  Logout
+                </Button>
+              </div>
+            </li>
+          </div>
         </nav>
-        <div className="collapse navbar-collapse">
-          <ul className="navbar-nav mr-auto">
-            <li className={`nav-item${getNavLinkClass('/')}`}>
-              <Button className="nav-link" type="link" href="/">
-                Beranda
-              </Button>
-            </li>
-            <li className={`nav-item${getNavLinkClass('/pencarian')}`}>
-              <Button className="nav-link" type="link" href="/pencarian">
-                Pencarian
-              </Button>
-            </li>
-            <li className={`nav-item${getNavLinkClass('/tentang')}`}>
-              <Button className="nav-link" type="link" href="/tentang">
-                Tentang
-              </Button>
-            </li>
-          </ul>
-          <li className="nav-item dropdown">
-            <a
-              className="nav-link dropdown-toggle"
-              href="#"
-              role="button"
-              data-toggle="dropdown"
-              aria-expanded="false"
-            >
-              Dropdown
-            </a>
-            <div className="dropdown-menu">
-              <Button
-                className={`dropdown-item${getNavLinkClass('/history')}`}
-                type="link"
-                href="/history"
-              >
-                History
-              </Button>
-              <Button
-                className={`dropdown-item${getNavLinkClass('/logout')}`}
-                type="link"
-                href="/logout"
-              >
-                Logout
-              </Button>
-            </div>
-          </li>
-        </div>
       </div>
     </header>
   );
