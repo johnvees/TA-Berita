42a336f2f35bdbce55f1ef9929c6944874f29c2a
Yohanes Velly <yohanesvelly23@gmail.com>
2023-05-24 01:49:11 +0700

    restyling for wrong center placement

--- src/parts/Landing/Hero.js
+++ src/parts/Landing/Hero.js
@@ -6,23 +6,26 @@ export default function Hero() {
     <section className="container-fluid">
       <div className="row">
         <div className="col-sm-2 col-md-2 col-lg-2 col-xl-2"></div>
-        <div className="hero-landing col-sm-8 col-md-8 col-lg-8 col-xl-8">
-          <h1>SISTEM KURATOR BERITA GRATIS UNTUK ANDA</h1>
-          <p>
-            Saat ini telah banyak berita yang dipublikasikan baik melalui media
-            digital maupun cetak. Namun apakah berita yang anda cari selama ini
-            sesuai dengan berita yang hendak anda cari? Kami disini hadir untuk
-            memastikan berita yang anda baca sesuai dengan yang anda inginkan.{' '}
-          </p>
+        <div className="hero col-sm-8 col-md-8 col-lg-8 col-xl-8">
+          <div className="hero-landing">
+            <h1>SISTEM KURATOR BERITA GRATIS UNTUK ANDA</h1>
+            <p>
+              Saat ini telah banyak berita yang dipublikasikan baik melalui
+              media digital maupun cetak. Namun apakah berita yang anda cari
+              selama ini sesuai dengan berita yang hendak anda cari? Kami disini
+              hadir untuk memastikan berita yang anda baca sesuai dengan yang
+              anda inginkan.{' '}
+            </p>
+          </div>
 
           <div className="row">
-            <div className="col-sm-2 col-md-2 col-lg-2 col-xl-2"></div>
-            <div className="col-sm-8 col-md-8 col-lg-8 col-xl-8">
+            <div className="col-sm-3 col-md-3 col-lg-3 col-xl-3"></div>
+            <div className="col-sm-6 col-md-6 col-lg-6 col-xl-6">
               <Button className="btn btn-mulai" type="link" href="/pencarian">
                 Mulai Sekarang
               </Button>
             </div>
-            <div className="col-sm-2 col-md-2 col-lg-2 col-xl-2"></div>
+            <div className="col-sm-3 col-md-3 col-lg-3 col-xl-3"></div>
           </div>
         </div>
         <div className="col-sm-2 col-md-2 col-lg-2 col-xl-2"></div>
