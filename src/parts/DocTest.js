d26ce8f204c2e268f44a89a9204047bf013eb7be
Yohanes Velly <yohanesvelly23@gmail.com>
2023-06-09 12:07:40 +0700

    add dokumen test

--- src/parts/DocTest.js
+++ src/parts/DocTest.js
@@ -1,73 +1,97 @@
 import React, {useState} from 'react'
 import JSZip from 'jszip';
-import { parseString } from 'xml2js';
+import JSZipUtils from 'jszip-utils';
+import PizZip from 'pizzip';
+import { Document, Packer } from 'docx';
 
 export default function DocTest() {
   const [selectedFiles, setSelectedFiles] = useState([]);
   const [fileContent, setFileContent] = useState('');
 
- const handleFileSelect = async (event) => {
-   const file = event.target.files[0];
+  const handleFileSelect = async (event) => {
+    const file = event.target.files[0];
 
-   if (file) {
-     const result = await convertToText(file);
-     setFileContent(result);
-   }
- };
+    if (file) {
+      const result = await readTextFile(file);
+      setFileContent(result);
+    }
+  };
 
- const convertToText = async (file) => {
-   const reader = new FileReader();
+  const readTextFile = async (file) => {
+    const reader = new FileReader();
 
-   return new Promise((resolve, reject) => {
-     reader.onload = async (event) => {
-       const arrayBuffer = event.target.result;
+    return new Promise((resolve, reject) => {
+      reader.onload = (event) => {
+        const content = event.target.result;
+        resolve(content);
+      };
 
-       try {
-         const zip = await JSZip.loadAsync(arrayBuffer);
-         const content = await zip.file('word/document.xml').async('string');
-         const parsedContent = await parseXml(content);
-         const text = extractTextFromXml(parsedContent);
+      reader.onerror = (event) => {
+        reject(event.target.error);
+      };
 
-         resolve(text);
-       } catch (error) {
-         reject(error);
-       }
-     };
+      reader.readAsText(file);
+    });
+  };
 
-     reader.onerror = (event) => {
-       reject(event.target.error);
-     };
+  const readDocxFile = async (file) => {
+    const reader = new FileReader();
 
-     reader.readAsArrayBuffer(file);
-   });
- };
+    return new Promise((resolve, reject) => {
+      reader.onload = async (event) => {
+        const arrayBuffer = event.target.result;
 
- const parseXml = (xml) => {
-   return new Promise((resolve, reject) => {
-     parseString(xml, (error, result) => {
-       if (error) {
-         reject(error);
-       } else {
-         resolve(result);
-       }
-     });
-   });
- };
+        try {
+          const zip = await JSZip.loadAsync(arrayBuffer);
+          const contentXml = await zip
+            .file('word/document.xml')
+            .async('string');
+          const doc = new DOMParser().parseFromString(contentXml, 'text/xml');
+          const textNodes = doc.getElementsByTagName('w:t');
+          let text = '';
 
- const extractTextFromXml = (xml) => {
-   // Modify this function to extract the desired text from the parsed XML structure
-   // This example simply returns the raw XML content as text
-   return JSON.stringify(xml);
- };
+          for (let i = 0; i < textNodes.length; i++) {
+            text += textNodes[i].textContent;
+          }
 
- return (
-   <div>
-     <input type="file" onChange={handleFileSelect} />
+          resolve(text);
+        } catch (error) {
+          reject(error);
+        }
+      };
 
-     <div>
-       <h2>File Content:</h2>
-       <pre>{fileContent}</pre>
-     </div>
-   </div>
- );
+      reader.onerror = (event) => {
+        reject(event.target.error);
+      };
+
+      reader.readAsArrayBuffer(file);
+    });
+  };
+
+  const handleFileContent = async (file) => {
+    if (file.name.endsWith('.docx')) {
+      const result = await readDocxFile(file);
+      setFileContent(result);
+    } else if (file.name.endsWith('.txt')) {
+      const result = await readTextFile(file);
+      setFileContent(result);
+    } else {
+      setFileContent('');
+    }
+  };
+
+  return (
+    <div>
+      <input
+        type="file"
+        accept=".docx, .txt"
+        onChange={(e) => handleFileContent(e.target.files[0])}
+      />
+
+      <div>
+        <h2>File Content:</h2>
+        <pre>{fileContent}</pre>
+      </div>
+    </div>
+  );
 }
